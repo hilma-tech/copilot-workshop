2fae7168b70c6eb0dfe87dd6d39829c06d3509c4
Shani Kehati <56911506+shanike@users.noreply.github.com>
2025-05-29 15:34:52 +0300

    fix: uncomment code

--- ex-n/chat-1.js
+++ ex-n/chat-1.js
@@ -1,5 +1,5 @@
 async function processUsers(users) {
-    // for (const user of users) {
-    //     await sendEmail(user);
-    // }
+    for (const user of users) {
+        await sendEmail(user);
+    }
 }
